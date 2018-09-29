efd43ed1198d6b1c6d03c329b1f9cfc3e9560d62
Carlos Vega <dnaatwork@gmail.com>
2018-09-29 09:23:01 -0600

    Create graphemeScripts.js

---
+++ graphemeScripts.js
@@ -0,0 +1,313 @@
+var instance = new Mark(document.querySelector("color"));
+
+
+instance.mark("a", {
+  "element": "span",
+  "className": "a",
+  caseSensitive: true,
+});
+instance.mark("b", {
+  "element": "span",
+  "className": "b",
+  caseSensitive: true,
+});
+instance.mark("c", {
+  "element": "span",
+  "className": "c",
+  caseSensitive: true,
+});
+instance.mark("d", {
+  "element": "span",
+  "className": "d",
+  caseSensitive: true,
+});
+instance.mark("e", {
+  "element": "span",
+  "className": "e",
+  caseSensitive: true,
+});
+instance.mark("f", {
+  "element": "span",
+  "className": "f",
+  caseSensitive: true,
+});
+instance.mark("g", {
+  "element": "span",
+  "className": "g",
+  caseSensitive: true,
+});
+instance.mark("h", {
+  "element": "span",
+  "className": "h",
+  caseSensitive: true,
+});
+instance.mark("i", {
+  "element": "span",
+  "className": "i",
+  caseSensitive: true,
+});
+instance.mark("j", {
+  "element": "span",
+  "className": "j",
+  caseSensitive: true,
+});
+instance.mark("k", {
+  "element": "span",
+  "className": "k",
+  caseSensitive: true,
+});
+instance.mark("l", {
+  "element": "span",
+  "className": "l",
+  caseSensitive: true,
+});
+instance.mark("m", {
+  "element": "span",
+  "className": "m",
+  caseSensitive: true,
+});
+instance.mark("n", {
+  "element": "span",
+  "className": "n",
+  caseSensitive: true,
+});
+instance.mark("o", {
+  "element": "span",
+  "className": "o",
+  caseSensitive: true,
+});
+instance.mark("p", {
+  "element": "span",
+  "className": "p",
+  caseSensitive: true,
+});
+instance.mark("q", {
+  "element": "span",
+  "className": "q",
+  caseSensitive: true,
+});
+instance.mark("r", {
+  "element": "span",
+  "className": "r",
+  caseSensitive: true,
+});
+instance.mark("s", {
+  "element": "span",
+  "className": "s",
+  caseSensitive: true,
+});
+instance.mark("t", {
+  "element": "span",
+  "className": "t",
+  caseSensitive: true,
+});
+instance.mark("u", {
+  "element": "span",
+  "className": "u",
+  caseSensitive: true,
+});
+instance.mark("v", {
+  "element": "span",
+  "className": "v",
+  caseSensitive: true,
+});
+instance.mark("w", {
+  "element": "span",
+  "className": "w",
+  caseSensitive: true,
+});
+instance.mark("x", {
+  "element": "span",
+  "className": "x",
+  caseSensitive: true,
+});
+instance.mark("y", {
+  "element": "span",
+  "className": "y",
+  caseSensitive: true,
+});
+instance.mark("z", {
+  "element": "span",
+  "className": "z",
+  caseSensitive: true,
+});
+instance.mark("A", {
+  "element": "span",
+  "className": "A",
+  caseSensitive: true,
+});
+instance.mark("B", {
+  "element": "span",
+  "className": "B",
+  caseSensitive: true,
+});
+instance.mark("C", {
+  "element": "span",
+  "className": "C",
+  caseSensitive: true,
+});
+instance.mark("D", {
+  "element": "span",
+  "className": "D",
+  caseSensitive: true,
+});
+instance.mark("E", {
+  "element": "span",
+  "className": "E",
+  caseSensitive: true,
+});
+instance.mark("F", {
+  "element": "span",
+  "className": "F",
+  caseSensitive: true,
+});
+instance.mark("G", {
+  "element": "span",
+  "className": "G",
+  caseSensitive: true,
+});
+instance.mark("H", {
+  "element": "span",
+  "className": "H",
+  caseSensitive: true,
+});
+instance.mark("I", {
+  "element": "span",
+  "className": "I",
+  caseSensitive: true,
+});
+instance.mark("J", {
+  "element": "span",
+  "className": "J",
+  caseSensitive: true,
+});
+instance.mark("K", {
+  "element": "span",
+  "className": "K",
+  caseSensitive: true,
+});
+instance.mark("L", {
+  "element": "span",
+  "className": "L",
+  caseSensitive: true,
+});
+instance.mark("M", {
+  "element": "span",
+  "className": "M",
+  caseSensitive: true,
+});
+instance.mark("N", {
+  "element": "span",
+  "className": "N",
+  caseSensitive: true,
+});
+instance.mark("O", {
+  "element": "span",
+  "className": "O",
+  caseSensitive: true,
+});
+instance.mark("P", {
+  "element": "span",
+  "className": "P",
+  caseSensitive: true,
+});
+instance.mark("Q", {
+  "element": "span",
+  "className": "Q",
+  caseSensitive: true,
+});
+instance.mark("R", {
+  "element": "span",
+  "className": "R",
+  caseSensitive: true,
+});
+instance.mark("S", {
+  "element": "span",
+  "className": "S",
+  caseSensitive: true,
+});
+instance.mark("T", {
+  "element": "span",
+  "className": "T",
+  caseSensitive: true,
+});
+instance.mark("U", {
+  "element": "span",
+  "className": "U",
+  caseSensitive: true,
+});
+instance.mark("V", {
+  "element": "span",
+  "className": "V",
+  caseSensitive: true,
+});
+instance.mark("W", {
+  "element": "span",
+  "className": "W",
+  caseSensitive: true,
+});
+instance.mark("X", {
+  "element": "span",
+  "className": "X",
+  caseSensitive: true,
+});
+instance.mark("Y", {
+  "element": "span",
+  "className": "Y",
+  caseSensitive: true,
+});
+instance.mark("Z", {
+  "element": "span",
+  "className": "Z",
+  caseSensitive: true,
+});
+instance.mark("1", {
+  "element": "span",
+  "className": "_1",
+  caseSensitive: true,
+});
+instance.mark("2", {
+  "element": "span",
+  "className": "_2",
+  caseSensitive: true,
+});
+instance.mark("3", {
+  "element": "span",
+  "className": "_3",
+  caseSensitive: true,
+});
+instance.mark("4", {
+  "element": "span",
+  "className": "_4",
+  caseSensitive: true,
+});
+instance.mark("5", {
+  "element": "span",
+  "className": "_5",
+  caseSensitive: true,
+});
+instance.mark("6", {
+  "element": "span",
+  "className": "_6",
+  caseSensitive: true,
+});
+instance.mark("7", {
+  "element": "span",
+  "className": "_7",
+  caseSensitive: true,
+});
+instance.mark("8", {
+  "element": "span",
+  "className": "_8",
+  caseSensitive: true,
+});
+instance.mark("9", {
+  "element": "span",
+  "className": "_9",
+  caseSensitive: true,
+});
+instance.mark("0", {
+  "element": "span",
+  "className": "_0",
+  caseSensitive: true,
+});
